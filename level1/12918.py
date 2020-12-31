2c17dd92093798902b42fdf2dbd601a47fd15ceb
unknown <package9@naver.com>
2020-12-31 10:25:04 +0900

    문자열 다루기 기본

--- level1/12918.py
+++ level1/12918.py
@@ -1,1 +1,8 @@
-"a".lower()
+def solution(s):
+    if len(s) == 4 or len(s) == 6:
+        for i in range(len(s)):
+            if s[i].isalpha() == True:
+                return False
+        return True
+    else:
+        return False
