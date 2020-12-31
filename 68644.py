d0251eb80005cdf907bccad2cb8255cba4be86cb
unknown <package9@naver.com>
2020-12-31 10:21:26 +0900

    두 개 뽑아서 더하기(python)

---
+++ 68644.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+
+
+def solution(numbers):
+    answer = []
+    a = list(combinations(numbers, 2))
+    for i in a:
+        answer.append(sum(i))
+    a = list(set(answer))
+    a.sort()
+    return a
