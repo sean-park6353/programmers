a5cbe45bc4edcec7411c426f9ed18be79e7a4b81
unknown <package9@naver.com>
2020-12-31 10:20:45 +0900

    두 개 뽑아서 더하기(node)

---
+++ 68644.js
@@ -0,0 +1,19 @@
+function solution(numbers) {
+    var answer = [];
+
+    for (var i = 0; i < numbers.length; i++) {
+        for (var j = 0; j < i + 1; j++) {
+            if (i !== j) {
+                answer.push(numbers[i] + numbers[j]);
+            }
+        }
+    }
+
+    answer = Array.from(new Set(answer));
+
+    answer.sort(function (a, b) {
+        return a - b;
+    });
+
+    return answer;
+}
