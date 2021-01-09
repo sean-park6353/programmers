327e75461eb6e064bd992aea41c1544145d4ad2a
unknown <package9@naver.com>
2021-01-09 23:39:42 +0900

    전화번호 목록

---
+++ level2/42577.py
@@ -0,0 +1,7 @@
+def solution(phone_book):
+    for i in range(len(phone_book)):
+        for j in range(len(phone_book)):
+            if i != j:
+                if phone_book[i] == phone_book[j][0:len(phone_book[i])]:
+                    return False
+    return True
