81fbf042d6b89d48ba3e860b6204a4692b413519
unknown <package9@naver.com>
2021-01-09 23:49:35 +0900

    전화번호 목록

--- level2/42577.py
+++ level2/42577.py
@@ -1,7 +1,17 @@
 def solution(phone_book):
-    for i in range(len(phone_book)):
-        for j in range(len(phone_book)):
+    for i in phone_book:
+        for j in phone_book:
             if i != j:
-                if phone_book[i] == phone_book[j][0:len(phone_book[i])]:
+                if i.startswith(j):
                     return False
     return True
+
+
+# def solution(phone_book):
+#     for i in range(len(phone_book)):
+#         for j in range(len(phone_book)):
+#             if i != j:
+#                 if phone_book[i] == phone_book[j][0:len(phone_book[i])]:
+#                     return False
+
+#     return True
---
+++ level2/연습장.py
@@ -0,0 +1,4 @@
+a = "12345"
+b = "123"
+
+print(a.startswith(b))
