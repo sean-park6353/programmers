447fd3f57a8e31bc506e88273622d61634cc6ab9
unknown <package9@naver.com>
2021-01-28 23:51:41 +0900

    다리를 지나는 트럭

--- level2/42583.py
+++ level2/42583.py
@@ -1,20 +1,20 @@
-from collections import deque
-
-
-
-def solution(bridge_length, weight, truck_weights):
+def solution(bridge_length,weight,truck_weights):
     time=0
-    truck_weights=deque(truck_weights)
-    bridge= deque([0 for _ in range(bridge_length)])
-    while len(bridge)!=0:
+    bridge=[0 for _ in range(bridge_length)]
+    while bridge:
         time+=1
-        bridge.popleft()
+        bridge.pop(0)
         if truck_weights:
             if sum(bridge)+truck_weights[0]<=weight:
-                bridge.append(truck_weights.popleft())
+                bridge.append(truck_weights.pop(0))
             else:
                 bridge.append(0)
     return time
 
 
+
+
+
 print(solution(2,10,[7,4,5,6]))
+
+
